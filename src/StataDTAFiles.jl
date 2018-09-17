3322808cccbb2ad3eb987de38b62a3209df99f79
Tamas K. Papp <tkpapp@gmail.com>
2018-09-17 17:20:33 +0200

    reworked tag reading, read header

--- src/StataDTAFiles.jl
+++ src/StataDTAFiles.jl
@@ -6,7 +6,7 @@ using DocStringExtensions: SIGNATURES
 """
 $(SIGNATURES)
 
-Read `"<tag>"` and `"</tag>"` from `io` as `"tag", false` or `"tag", true`, respectively.
+Read `"<tag>"` from `io` as `"tag"`. For closing tags, the initial `'/'` is included.
 
 Throw an error after `maxbytes` bytes are read.
 """
@@ -15,13 +15,10 @@ function readtag(io::IO; maxbytes = 128)
     lt == UInt8('<') || error("First byte is not a '<'.")
     content = Vector{UInt8}()
     bytecount = 0
-    isclosing = false
     while !eof(io) && bytecount < maxbytes
         c = read(io, UInt8)
-        if bytecount == 0 && c == UInt8('/')
-            isclosing = true
-        elseif c == UInt8('>')
-            return String(content), isclosing
+        if c == UInt8('>')
+            return String(content)
         else
             push!(content, c)
             bytecount += 1
@@ -29,6 +26,73 @@ function readtag(io::IO; maxbytes = 128)
     end
     msg = eof(io) ? "Reached end of file" : "Read $(bytecount) bytes"
     error("$(msg) without finding a closing '>'.")
+end
+
+## FIXME this could be much faster, not required to read
+verifytag(io::IO, tag, closing = false) = @assert readtag(io) == (closing ? "/" * tag : tag)
+
+function verifytag(f, io::IO, tag)
+    verifytag(io, tag)
+    result = f(io)
+    verifytag(io, tag, true)
+    result
+end
+
+readfixed(io::IO, nb) = String(read!(io, Vector{UInt8}(undef, nb)))
+
+abstract type ByteOrder end
+
+struct MSF <: ByteOrder end     # big-endian
+
+struct LSF <: ByteOrder end     # little-endian
+
+readnum(io::IO, ::MSF, T) = ntoh(read(io, T))
+
+readnum(io::IO, ::LSF, T) = ltoh(read(io, T))
+
+function readbyteorder(io::IO)
+    verifytag(io, "byteorder") do io
+        order = readfixed(io, 3)
+        if order == "MSF"
+            MSF()
+        elseif order == "LSF"
+            LSF()
+        else
+            error("unknown byte order $(order)")
+        end
+    end
+end
+
+function readstrfs(io::IO, byteorder, T)
+    len = Int(readnum(io, byteorder, T))
+    buffer = Vector{UInt8}(undef, len)
+    read!(io, buffer)
+    numchars = findfirst(isequal(0x00), buffer)
+    if numchars ≢ nothing
+        resize!(buffer, numchars)
+    end
+    String(buffer)
+end
+
+struct DTAHeader{B <: ByteOrder}
+    release::Int
+    byteorder::B
+    columns::Int
+    observations::Int
+    label::String
+    timestamp::String           # FIXME parse date in timestamp
+end
+
+function readheader(io::IO)
+    verifytag(io, "header") do io
+        @assert verifytag(io -> readfixed(io, 3), io, "release") == "118"
+        byteorder = readbyteorder(io)
+        K = verifytag(io -> readnum(io, byteorder, Int16), io, "K")
+        N = verifytag(io -> readnum(io, byteorder, Int64), io, "N")
+        label = verifytag(io -> readstrfs(io, byteorder, Int16), io, "label")
+        timestamp = verifytag(io -> readstrfs(io, byteorder, Int8), io, "timestamp")
+        DTAHeader(118, byteorder, Int(K), Int(N), label, timestamp)
+    end
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,22 @@
-using StataDTAFiles
-using StataDTAFiles: readtag
-using Test
+using StataDTAFiles, Test
+using StataDTAFiles: readtag, verifytag, readheader, LSF
 
 @testset "reading tags" begin
-    @test readtag(IOBuffer("<atag>")) == ("atag", false)
-    @test readtag(IOBuffer("</btag>")) == ("btag", true)
+    @test readtag(IOBuffer("<atag>")) == "atag"
+    @test readtag(IOBuffer("</btag>")) == "/btag"
     @test_throws ErrorException readtag(IOBuffer("noopening>"))
     @test_throws ErrorException readtag(IOBuffer("<eof"))
     @test_throws ErrorException readtag(IOBuffer("<toolong"); maxbytes = 3)
+end
+
+@testset "reading header" begin
+    testdata = joinpath(@__DIR__, "data", "testdata.dta")
+    io = open(testdata, "r")
+    verifytag(io, "stata_dta")
+    hdr = readheader(io)
+    @test hdr.release == 118
+    @test hdr.byteorder ≡ LSF()
+    @test hdr.columns == 3
+    @test hdr.observations == 10
+    close(io)
 end
