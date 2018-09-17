ad8d604fa9e7dc85c4fc32e8ec45dac5a016d020
Tamas K. Papp <tkpapp@gmail.com>
2018-09-17 16:05:46 +0200

    Added tag reading.

--- src/StataDTAFiles.jl
+++ src/StataDTAFiles.jl
@@ -1,5 +1,34 @@
 module StataDTAFiles
 
-greet() = print("Hello World!")
+using ArgCheck: @argcheck
+using DocStringExtensions: SIGNATURES
+
+"""
+$(SIGNATURES)
+
+Read `"<tag>"` and `"</tag>"` from `io` as `"tag", false` or `"tag", true`, respectively.
+
+Throw an error after `maxbytes` bytes are read.
+"""
+function readtag(io::IO; maxbytes = 128)
+    lt = read(io, UInt8)
+    lt == UInt8('<') || error("First byte is not a '<'.")
+    content = Vector{UInt8}()
+    bytecount = 0
+    isclosing = false
+    while !eof(io) && bytecount < maxbytes
+        c = read(io, UInt8)
+        if bytecount == 0 && c == UInt8('/')
+            isclosing = true
+        elseif c == UInt8('>')
+            return String(content), isclosing
+        else
+            push!(content, c)
+            bytecount += 1
+        end
+    end
+    msg = eof(io) ? "Reached end of file" : "Read $(bytecount) bytes"
+    error("$(msg) without finding a closing '>'.")
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,11 @@
 using StataDTAFiles
+using StataDTAFiles: readtag
 using Test
+
+@testset "reading tags" begin
+    @test readtag(IOBuffer("<atag>")) == ("atag", false)
+    @test readtag(IOBuffer("</btag>")) == ("btag", true)
+    @test_throws ErrorException readtag(IOBuffer("noopening>"))
+    @test_throws ErrorException readtag(IOBuffer("<eof"))
+    @test_throws ErrorException readtag(IOBuffer("<toolong"); maxbytes = 3)
+end
